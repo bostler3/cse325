8c64e83997a0b81893d2e3f81324b033ac5bbfb4
bostler3 <bostler3@gmail.com>
2026-07-02 22:50:50 -0600

    W01 Cleanup

--- ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1feeb259b9c9f85fd61e7a497bbf68c25d4ec70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
